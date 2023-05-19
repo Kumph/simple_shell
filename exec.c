d86f875038df8fa7952f861b82e5efba98c992a0
Kumph <mphatsokumwendaec@gmail.com>
2023-05-19 06:38:06 -0700

    update execve to recognise path

--- exec.c
+++ exec.c
@@ -4,24 +4,26 @@
  * @command: command
  */
 
-void execute_command(char *command)
+void execute_command(char *cmd)
 {
 	int status;
 	pid_t pid = fork();
 	char *arguments[3];
+	char *path = NULL;
 
-	arguments[0] = command;
+	arguments[0] = cmd;
 	arguments[1] = NULL;
 
+	path = get_path(cmd);
 
 	if (pid == -1)
 	{
-		perror("Error");
+		perror("Error: fork failed");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
-		execve(command, arguments, NULL);
+		execve(path, arguments, NULL);
 		perror("Error");
 		exit(EXIT_FAILURE);
 
