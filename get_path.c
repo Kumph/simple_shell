ad8a438cad0c5d8bdb4c41899fcde03f327359aa
DeFatso <115782937+DeFatso@users.noreply.github.com>
2023-05-26 21:16:46 +0000

    exec.c

--- get_path.c
+++ get_path.c
@@ -3,49 +3,48 @@
 /**
  * get_path - searches directories in PATH for cmd's executable file
  * @cmd: command whose executable file to look for in PATH
- * Return: path on success or NULL if not found
+ * Return: path on sucess or NULL if not found
  */
 char *get_path(char *cmd)
 {
-	char *path_buffer;
-	char *path = _getenv("PATH");
-	char *p = strtok(path, ":");
-	char *result = NULL;
+	char *path = _getenv("PATH"), *p, *p2 = NULL, *path_buffer;
 	struct stat st;
 
+	p2 = malloc(_strlen(path) + 1);
+	if (p2 == NULL)
+	{
+		perror("Error: unable to allocate memory");
+		return (NULL);
+	}
+	strcpy(p2, path);
+	p = strtok(p2, ":");
+	free(p2);
 	while (p != NULL)
 	{
-		path_buffer = malloc((_strlen(p) + _strlen(cmd) + 2) * sizeof(char));
+		path_buffer = malloc((strlen(p) + strlen(cmd) + 2) * sizeof(char));
 		if (path_buffer == NULL)
 		{
 			perror("Error: unable to allocate memory");
-			free(path);
-			return NULL;
+			return (NULL);
 		}
-
 		strcpy(path_buffer, p);
-		_strcat(path_buffer, "/");
-		_strcat(path_buffer, cmd);
-
+		strcat(path_buffer, "/");
+		strcat(path_buffer, cmd);
 		if (stat(path_buffer, &st) == 0 && S_ISREG(st.st_mode))
 		{
-			result = malloc(_strlen(path_buffer) + 1);
+			char *result = malloc(_strlen(path_buffer) + 1);
 			if (result == NULL)
 			{
-				perror("Error: unable to allocate memory");
-				free(path_buffer);
-				free(path);
-				return NULL;
+				
+				return (NULL);
 			}
-
-			_strcpy(result, path_buffer);
-			break;
+			strcpy(result, path_buffer);
+			free(path_buffer);
+			return (result);
 		}
-
 		p = strtok(NULL, ":");
-		free(path_buffer);
+
 	}
-	free(path_buffer);
-	free(path);
-	return (result);
+	perror("Error: Command not found");
+	return (NULL);
 }
